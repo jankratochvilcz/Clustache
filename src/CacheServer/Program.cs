715d627eda5e4f7ecd5d0e02946deb2d9be282a7
Jan Kratochvil <kratochvil.jan@outlook.com>
2025-02-19 21:04:22 +0100

    Fixes Redis configuration

--- src/CacheServer/Program.cs
+++ src/CacheServer/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Clustache.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -6,13 +7,17 @@ using Prometheus;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
-builder.Services
-    .AddSingleton<CachedItemService>()
-    .AddSingleton(new RedisConfiguration { ConnectionString = "localhost:51160" })
+var redisPort = int.Parse(Environment.GetEnvironmentVariable("REDIS_PORT"));
+var redisPath = Environment.GetEnvironmentVariable("REDIS_PATH");
+string redisConnectionString = $"{redisPath}:{redisPort}";
+Console.WriteLine(redisConnectionString);
+
+builder
+    .Services.AddSingleton<CachedItemService>()
+    .AddSingleton(new RedisConfiguration { ConnectionString = redisConnectionString })
     .AddSingleton<RedisService>();
 
 builder.Services.AddHostedService<CachedItemConsumerService>();
-
 
 var app = builder.Build();
 
